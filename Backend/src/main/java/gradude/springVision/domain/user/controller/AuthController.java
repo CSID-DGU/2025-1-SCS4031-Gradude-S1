e3c05c899514fc03fbdedb7122a3663f282af33a
jjin70 <jjin0434@naver.com>
2025-04-18 00:33:40 +0900

    BE: [docs] 회원가입/로그인API Swagger 명세 수정 #3

--- Backend/src/main/java/gradude/springVision/domain/user/controller/AuthController.java
+++ Backend/src/main/java/gradude/springVision/domain/user/controller/AuthController.java
@@ -20,13 +20,13 @@ public class AuthController {
     private final AuthCommandService authCommandService;
 
     @Operation(summary = "카카오 로그인")
-    @Parameter(name = "code", description = "RequestParam, 카카오에서 발급 받은 인가코드")
+    @Parameter(name = "code", description = "query string parameter, 카카오에서 발급 받은 인가코드")
     @PostMapping("/login")
     public ApiResponse<LoginResponseDTO> kakaoLogin(@RequestParam("code") String code) {
         return ApiResponse.onSuccess(authCommandService.login(code));
     }
 
-    @Operation(summary = "회원가입", description = "최초 로그인일 경우: 추가 정보 기입 ")
+    @Operation(summary = "회원가입",description = "최초 로그인일 경우: 추가 정보 기입 <br> kakaoId, profileImageUrl은 카카오 로그인에서 받은 url")
     @PostMapping("/signup")
     public ApiResponse<SignupResponseDTO> signup(@RequestBody SignupRequestDTO signupRequestDTO) {
         return ApiResponse.onSuccess(authCommandService.signup(signupRequestDTO));
