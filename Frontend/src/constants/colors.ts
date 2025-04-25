59eb21d692c81fe2958d5b52d674df2e725cffe9
dahyeon <youlove7r@gmail.com>
2025-04-25 17:09:57 +0900

    FE: [feat] 프로젝트 구조 세팅 #10

---
+++ Frontend/src/constants/colors.ts
@@ -0,0 +1,13 @@
+const colors = {
+  SKYBLUE: '#72ADFF',
+  MAINBLUE: '#007AFE',
+  BLUE: '#6E95FB',
+  PURPLE: '#BE9FFF',
+  RED: '#FF1400',
+  BLACK: '#000000',
+  WHITE: '#FFFFFF',
+  SEMIWHITE: '#F9F9F9',
+  YELLOW: '#FEE500',
+};
+
+export default colors;
