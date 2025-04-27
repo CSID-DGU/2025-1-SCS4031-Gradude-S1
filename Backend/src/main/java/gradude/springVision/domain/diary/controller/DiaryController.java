bbc55ace2c9baeced42e3f2b8c63c72700567c73
jjin70 <jjin0434@naver.com>
2025-04-27 11:09:37 +0900

    BE: [fix] 조회 메서드 Get으로 수정 #6

--- Backend/src/main/java/gradude/springVision/domain/diary/controller/DiaryController.java
+++ Backend/src/main/java/gradude/springVision/domain/diary/controller/DiaryController.java
@@ -35,20 +35,20 @@ public class DiaryController {
 
     @Operation(summary = "하루 기록 조회")
     @Parameter(name = "diaryId", description = "하루 기록 생성 및 캘린더 조회에서 받은 diaryId")
-    @PostMapping("/{diaryId}")
+    @GetMapping("/{diaryId}")
     public ApiResponse<DiaryDetailResponseDTO> getDiary(@AuthenticationPrincipal Long userId, @PathVariable("diaryId") Long diaryId) {
         return ApiResponse.onSuccess(diaryQueryService.getDiary(userId, diaryId));
     }
 
     @Operation(summary = "하루 기록 캘린더 조회", description = "년/월 기준으로 하루 기록이 작성된 날짜와 해당 기록의 id 조회")
-    @PostMapping("/calendar")
+    @GetMapping("/calendar")
     public ApiResponse<List<DiaryCalendarResponseDTO>> getDiaryCalender(@AuthenticationPrincipal Long userId,
                                                                         @RequestParam int year, @RequestParam int month) {
         return ApiResponse.onSuccess(diaryQueryService.getDiaryCalendar(userId, year, month));
     }
 
     @Operation(summary = "건강 점수 그래프 조회", description = "최근 5개 하루 기록의 건강 점수 조회")
-    @PostMapping("/graph")
+    @GetMapping("/graph")
     public ApiResponse<List<DiaryGraphResponseDTO>> getDiaryGraph(@AuthenticationPrincipal Long userId) {
         return ApiResponse.onSuccess(diaryQueryService.getDiaryGraph(userId));
     }
