9ecdec1b173c594785af902dbb902e6036d01d9c
dahyeon <youlove7r@gmail.com>
2025-04-29 16:42:05 +0900

    FE: [feat] Splash Screen #14

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -1,12 +1,22 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import AuthStackNavigator from './src/navigations/stack/AuthStackNavigator';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import queryClient from './src/api/queryClient';
 import RootNavigator from './src/navigations/root/RootNavigator';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import {hideSplash, showSplash} from 'react-native-splash-view';
 
 function App() {
+  // 로그인 api 연동 시, 이거 지우고 root navi에 설정하기
+  showSplash();
+
+  useEffect(() => {
+    setTimeout(() => {
+      hideSplash();
+    }, 500);
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <SafeAreaProvider>
