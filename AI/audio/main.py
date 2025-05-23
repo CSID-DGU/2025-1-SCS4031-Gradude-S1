8763f9e9c81a6c50f65453c47b1c278f7e1efd3f
jjin70 <jjin0434@naver.com>
2025-05-24 03:39:52 +0900

    BE: [fix] audio model main.py 수정(파일 충돌 방지) #40

--- AI/audio/main.py
+++ AI/audio/main.py
@@ -2,21 +2,26 @@ from fastapi import FastAPI, UploadFile, File, HTTPException
 from pathlib import Path
 import shutil
 import traceback
+import uuid
 
 from disorder_audio import predict  
 
 app = FastAPI()
 
-@app.post("/predict")
-async def predict_audio(file: UploadFile = File(...)):
-    temp_path = f"temp_{file.filename}"
-    
-    # 업로드 파일 임시 저장
-    with open(temp_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-
+@app.post("/api/audio/predict")
+def predict_audio(file: UploadFile = File(...)):
     try:
-        prediction, probability = predict(temp_path, model_path="svm_model.pkl")
+        # 안전한 파일 이름 생성 (충돌 방지)
+        file_suffix = Path(file.filename).suffix or ".wav"
+        filename = f"temp_{uuid.uuid4().hex}{file_suffix}"
+        temp_path = Path(filename)
+
+         # 업로드 파일 임시 저장
+        with open(temp_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        # 예측 수행
+        prediction, probability = predict(str(temp_path), model_path="svm_model.pkl")
         return {
             "prediction": int(prediction),
             "probability": float(probability)
@@ -35,5 +40,6 @@ async def predict_audio(file: UploadFile = File(...)):
 
     finally:
         # 임시 파일 삭제
-        if Path(temp_path).exists():
-            Path(temp_path).unlink()
+        if temp_path.exists():
+            temp_path.unlink()
+            
