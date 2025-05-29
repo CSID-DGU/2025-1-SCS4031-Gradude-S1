e907451549a14e551c24d524de60d42e297fc1fb
dahyeon <youlove7r@gmail.com>
2025-05-29 12:51:13 +0900

    FE: [chore] Remove RNSound pod

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -5,18 +5,8 @@ import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import queryClient from './src/api/queryClient';
 import RootNavigator from './src/navigations/root/RootNavigator';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
-import {hideSplash, showSplash} from 'react-native-splash-view';
 
 function App() {
-  // 로그인 api 연동 시, 이거 지우고 root navi에 설정하기
-  showSplash();
-
-  useEffect(() => {
-    setTimeout(() => {
-      hideSplash();
-    }, 500);
-  }, []);
-
   return (
     <SafeAreaProvider>
       <QueryClientProvider client={queryClient}>
--- Frontend/src/navigations/root/RootNavigator.tsx
+++ Frontend/src/navigations/root/RootNavigator.tsx
@@ -1,5 +1,5 @@
 // src/navigations/root/RootNavigator.tsx
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, View, ActivityIndicator} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import AuthStackNavigator from '../stack/AuthStackNavigator';
@@ -8,56 +8,66 @@ import TapNavigator from '../tab/TabNavigator';
 import useAuth from '@/hooks/queries/useAuth';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {authNavigations} from '@/constants';
+import {hideSplash, showSplash} from 'react-native-splash-view';
 
 // 연동 시, 아래 코드 주석 해제
-// export type AuthStackParamList = {
-//   [authNavigations.SIGNUP]: {authCode: string};
-// };
+export type AuthStackParamList = {
+  [authNavigations.SIGNUP]: {authCode: string};
+};
 
-// const Stack = createNativeStackNavigator<AuthStackParamList>();
+const Stack = createNativeStackNavigator<AuthStackParamList>();
 
-// export default function RootNavigator() {
-//   const {isAuthenticated, isProfileComplete, isLoading} = useAuth();
+export default function RootNavigator() {
+  const {isAuthenticated, isProfileComplete, isLoading} = useAuth();
 
-//   // 0) 아직 토큰 확인 중
-//   if (isLoading) {
-//     return (
-//       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-//         <ActivityIndicator size="large" />
-//       </View>
-//     );
-//   }
+  showSplash();
 
-//   // 1) 토큰이 없으면 → 로그인/카카오로그인/회원가입 스택
-//   if (!isAuthenticated) {
-//     return <AuthStackNavigator />;
-//   }
+  useEffect(() => {
+    setTimeout(() => {
+      hideSplash();
+    }, 300);
+  }, []);
 
-//   // 2) 토큰은 있는데, 프로필(추가정보) 없으면 → SignupScreen
-//   if (!isProfileComplete) {
-//     return (
-//     <Stack.Navigator screenOptions={{headerShown: false}}>
-//       <Stack.Screen
-//         name={authNavigations.SIGNUP}
-//         component={SignupScreen}
-//         options={{
-//           headerTitle: '회원가입',
-//         }}
-//       />
-//     </Stack.Navigator>;
-//   }
+  // 0) 아직 토큰 확인 중
+  if (isLoading) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
 
-//   // 3) 모두 완료된 회원 → 메인 탭 네비게이터
-//   return <TapNavigator />;
-// }
+  // 1) 토큰이 없으면 → 로그인/카카오로그인/회원가입 스택
+  if (!isAuthenticated) {
+    return <AuthStackNavigator />;
+  }
+
+  // 2) 토큰은 있는데, 프로필(추가정보) 없으면 → SignupScreen
+  if (!isProfileComplete) {
+    return (
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen
+          name={authNavigations.SIGNUP}
+          component={SignupScreen}
+          options={{
+            headerTitle: '회원가입',
+          }}
+        />
+      </Stack.Navigator>
+    );
+  }
+
+  // 3) 모두 완료된 회원 → 메인 탭 네비게이터
+  return <TapNavigator />;
+}
 
 // 아래 코드로 테스트
 
-export default RootNavigator;
-function RootNavigator() {
-  const isLogin = true;
-  if (isLogin === undefined) {
-    return <Text>로딩중…</Text>;
-  }
-  return isLogin ? <TapNavigator /> : <AuthStackNavigator />;
-}
+// export default RootNavigator;
+// function RootNavigator() {
+//   const isLogin = true;
+//   if (isLogin === undefined) {
+//     return <Text>로딩중…</Text>;
+//   }
+//   return isLogin ? <TapNavigator /> : <AuthStackNavigator />;
+// }
