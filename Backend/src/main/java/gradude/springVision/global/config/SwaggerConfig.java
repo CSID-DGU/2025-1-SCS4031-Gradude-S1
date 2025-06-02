93d32d32f585348601a835c3f4fa6a34682a2fc3
jjin70 <jjin0434@naver.com>
2025-06-03 01:01:21 +0900

    BE: [fix] Swagger 서버 URL 설정 추가 #80

--- Backend/src/main/java/gradude/springVision/global/config/SwaggerConfig.java
+++ Backend/src/main/java/gradude/springVision/global/config/SwaggerConfig.java
@@ -5,15 +5,36 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${backend.local}")
+    private String backendLocal;
+    @Value("${backend.dev-http}")
+    private String backendDevHttp;
+    @Value("${backend.dev-https}")
+    private String backendDevHttps;
+
     @Bean
     public OpenAPI openAPI() {
+        Server localServer = new Server();
+        localServer.setUrl(backendLocal);
+
+        Server devHttpServer = new Server();
+        devHttpServer.setUrl(backendDevHttp);
+
+        Server devHttpsServer = new Server();
+        devHttpsServer.setUrl(backendDevHttps);
+
         return new OpenAPI()
+                .servers(List.of(localServer, devHttpServer, devHttpsServer))
                 .components(new Components()
                         .addSecuritySchemes("Authorization", securityScheme()))
                 .addSecurityItem(new SecurityRequirement().addList("Authorization"))
